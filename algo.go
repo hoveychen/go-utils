5bdecbd2d383337b2aa76b0210dd58250fd91578
Yuheng Chen <hoveychen@gmail.com>
2016-08-30 13:43:26 +0800

    Add IterStringIntMap to iterate a map in key order.

--- algo.go
+++ algo.go
@@ -1,5 +1,9 @@
 package goutils
 
+import (
+	"sort"
+)
+
 func DedupStrings(slice []string) []string {
 	m := map[string]bool{}
 	for _, i := range slice {
@@ -13,5 +17,28 @@ func DedupStrings(slice []string) []string {
 		i++
 	}
 
+	return ret
+}
+
+type StringIntPair struct {
+	Key   string
+	Value int
+}
+
+func IterStringIntMap(in map[string]int) []StringIntPair {
+	keys := make([]string, len(in))
+	i := 0
+	for key, _ := range in {
+		keys[i] = key
+		i++
+	}
+
+	sort.Strings(keys)
+	ret := make([]StringIntPair, len(keys))
+	i = 0
+	for _, key := range keys {
+		ret[i] = StringIntPair{key, in[key]}
+		i++
+	}
 	return ret
 }
