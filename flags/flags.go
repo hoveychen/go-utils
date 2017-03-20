46f4cd1fb4d4392caa65def1e8f09b57d016ea5e
Yuheng Chen <hoveychen@gmail.com>
2017-03-20 18:34:36 +0800

    [Flags] Add string slice type.

--- flags/flags.go
+++ flags/flags.go
@@ -2,6 +2,7 @@ package flags
 
 import (
 	"flag"
+	"strings"
 	"time"
 )
 
@@ -11,7 +12,21 @@ var (
 	durationPtrs = map[string]*time.Duration{}
 	float64Ptrs  = map[string]*float64{}
 	intPtrs      = map[string]*int{}
+	slicePtrs    = map[string]*[]string{}
 )
+
+type sliceContainer struct {
+	S *[]string
+}
+
+func (s *sliceContainer) String() string {
+	return strings.Join(*s.S, ",")
+}
+
+func (s *sliceContainer) Set(value string) error {
+	*s.S = strings.Split(value, ",")
+	return nil
+}
 
 func String(name string, value string, usage string) *string {
 	if _, exists := stringPtrs[name]; !exists {
@@ -43,6 +58,22 @@ func Float64(name string, value float64, usage string) *float64 {
 	}
 
 	return float64Ptrs[name]
+}
+
+func Slice(name string, value []string, usage string) *[]string {
+	if _, exists := slicePtrs[name]; !exists {
+		var s []string
+		if value == nil {
+			s = []string{}
+		} else {
+			s = value
+		}
+		slicePtrs[name] = &s
+
+		container := sliceContainer{S: &s}
+		flag.Var(&container, name, usage)
+	}
+	return slicePtrs[name]
 }
 
 func Duration(name string, value time.Duration, usage string) *time.Duration {
